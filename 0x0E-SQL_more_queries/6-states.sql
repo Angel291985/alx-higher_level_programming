731f4129dbbaedbbfd0eadd1c9143124230c6963
Angel291985 <angelsiluma.03@gmail.com>
2023-03-15 18:31:08 +0200

    SQL more queries

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,13 @@
+-- The script that creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create table 'states' in db 'hbtn_0d_usa'
+-- id INT unique auto-generated not null and primary key
+-- name VARCHAR(256) not null
+-- if table already exists, script should not fail
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+);
