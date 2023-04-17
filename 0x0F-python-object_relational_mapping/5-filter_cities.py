4d12b72fa6be2a4e0bdde633056401cc957cbdf8
Angel291985 <angelsiluma.03@gmail.com>
2023-04-17 15:26:19 +0200

    python object

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
     ORDER BY cities.id", (argv[4],))
     rows = cursor.fetchall()
-    print(",".join(city[0] for city in rows))
+    print(", ".join(city[0] for city in rows))
     cursor.close()
     db.close()
